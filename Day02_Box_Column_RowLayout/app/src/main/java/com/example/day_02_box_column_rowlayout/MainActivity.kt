1e0a63d861324df4cb9cad752e364fdaf51da5dd
SSWARAJ1508 <Shubhamswaraj1508@gmail.com>
2026-01-26 13:39:51 +0530

    Day 02: Box, Row, and Column layout implementation

---
+++ Day02_Box_Column_RowLayout/app/src/main/java/com/example/day_02_box_column_rowlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.day_02_box_column_rowlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
